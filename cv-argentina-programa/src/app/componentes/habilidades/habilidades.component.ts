b3fb951762229d7ab4d8ba5697e6c1e4dc83b9fe
Gian <gianfrancoviola98@gmail.com>
2022-06-09 19:17:30 -0300

    ultima actualizacion

--- cv-argentina-programa/src/app/componentes/habilidades/habilidades.component.ts
+++ cv-argentina-programa/src/app/componentes/habilidades/habilidades.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./habilidades.component.css']
 })
 export class HabilidadesComponent implements OnInit {
+  
 
   constructor() { }
 
